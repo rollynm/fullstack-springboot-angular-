f9285465606e7b5f9ec8b7b8665d696dd47569a1
fluent <82741704+rollynm@users.noreply.github.com>
2023-05-28 21:03:51 +0800

    angular frontend

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { User } from '../user';
+import { UserServiceService } from '../user-service.service';
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent {
+  users: User[] = [];
+
+  constructor(private userService: UserServiceService) {
+  }
+
+  ngOnInit() {
+    this.userService.findAll().subscribe(data => {
+      this.users = data;
+    });
+  }
+}
